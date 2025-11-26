97af332140b6e8ba0c844e5958cd263ea16fb69b
deepakkroxford <deepakkr.oxford@gmail.com>
2025-11-26 08:10:09 +0530

    adding the strategy design pattern

---
+++ StrategyDesignPattern/WithoutStrategyPattern.java
@@ -0,0 +1,41 @@
+package StrategyDesignPattern;
+
+class Vehicle {
+    public void drive(){
+        System.out.println("Drive method" + this.getClass().getSimpleName());
+    }
+}
+
+
+class SportsCar extends Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("Driving a Sports Car");
+    }
+}
+
+class EvCar extends Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("Driving an Electric Car");
+    }
+}
+
+class Truck extends Vehicle {
+    @Override
+    public void drive() {
+        System.out.println("Driving a Truck");
+    }
+}
+public class WithoutStrategyPattern {
+    public static void main(String[] args) {
+        Vehicle sportsCar = new SportsCar();
+        sportsCar.drive();
+
+        Vehicle evCar = new EvCar();
+        evCar.drive();
+
+        Vehicle truck = new Truck();
+        truck.drive();
+    }
+}
---
+++ StrategyDesignPattern/implementation.java
@@ -0,0 +1,55 @@
+package StrategyDesignPattern;
+
+interface PaymentStrategy {
+    void pay(double amount);
+}
+class CreditCardPayment implements PaymentStrategy {
+    private String cardNumber;
+    private String cardHolderName;
+
+    public CreditCardPayment(String cardNumber, String cardHolderName) {
+        this.cardNumber = cardNumber;
+        this.cardHolderName = cardHolderName;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using Credit Card.");
+    }
+}
+class UpiPayment implements PaymentStrategy {
+
+    private String upiId;
+
+    public UpiPayment(String upiId) {
+        this.upiId = upiId;
+    }
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("Paid " + amount + " using UPI.");
+    }
+}
+class PaymentContext {
+    private PaymentStrategy paymentStrategy;
+
+    public PaymentContext(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void setPaymentStrategy(PaymentStrategy paymentStrategy) {
+        this.paymentStrategy = paymentStrategy;
+    }
+
+    public void executePayment(double amount) {
+        paymentStrategy.pay(amount);
+    }
+}
+public class implementation {
+    public static void main(String[] args) {
+        PaymentContext context = new PaymentContext(new CreditCardPayment("1234-5678-9012-3456", "John Doe"));
+        context.executePayment(100.0);
+        context = new PaymentContext(new UpiPayment("john@upi"));
+        context.executePayment(200.0);
+    }
+}
